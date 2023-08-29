9d872824f5474610a9bf2c1fee0edfce85173d76
Marcin Bielak <marcin.bieli@gmail.com>
2023-08-29 02:01:34 +0200

    add public trait IoTextData to library & refactoring code

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,21 +1,15 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-//use self::criterion::{Bencher, Criterion, Benchmark, PlotConfiguration, AxisScale};
-use iotext_rs::parse_iotext_str;
+use iotext_rs::IoTextData;
+use iotext_rs::IoTextDataRow;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    let data_obj: IoTextDataRow = IoTextDataRow::default();
     const MSG_EXAMPLE: &str = "t|3900237526042,d|device_name_001,m|val_water_001=i:1234,m|val_water_002=i:15,m|bulb_state=b:1,m|connector_state=b:0,m|temp_01=d:34.4,m|temp_02=d:36.4,m|temp_03=d:10.4,m|pwr=d:12.231,m|current=d:1.429,m|current_battery=d:1.548";
 
     c.bench_function("parse_iotext_str", |b| {
-        b.iter(|| parse_iotext_str(MSG_EXAMPLE))
+        b.iter(|| data_obj.parse_iotext_str(MSG_EXAMPLE))
     });
 }
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
-
-//let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
-
-// Using Criterion::default() for simplicity; normally you'd use the macros.
-//let mut criterion = Criterion::default();
-//let mut benchmark_group = criterion.benchmark_group("Group name");
-//benchmark_group.plot_config(plot_config);
